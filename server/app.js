bcaba94b75a1e1361faa0190a8a56e5a7d9aed26
Mark Kravchuk <markkravchuk123@icloud.com>
2024-09-25 17:46:28 -0700

    Connect to the database

--- server/app.js
+++ server/app.js
@@ -2,6 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.MONGO_CONNECTION_STRING;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 app.get("/", (req, res) => res.send("Hello, world!"));
 
 const PORT = process.env.PORT || 3000;
