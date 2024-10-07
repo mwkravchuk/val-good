9c8c972ba9b2b997953bc09ca20ff536c45b9752
Mark Kravchuk <markkravchuk123@icloud.com>
2024-10-06 17:29:44 -0700

    Add styling for matches

--- client/src/components/pages/stats/MatchItem.jsx
+++ client/src/components/pages/stats/MatchItem.jsx
@@ -1,8 +1,18 @@
 import styles from "./MatchItem.module.css";
 
 const MatchItem = ({ match }) => {
+
+  const isVictory = (match) => {
+    const selfTeam = match.stats.team;
+    const winningTeam = match.teams.blue > match.teams.red ? "Blue" : "Red";
+    return match.meta.mode === "Deathmatch" || selfTeam === winningTeam;
+  }
+
+  const victory = isVictory(match);
+
   return (
-    <li className={styles.matchContainer}>
+    <li className={`${styles.matchContainer} ${victory ? styles.matchVictory : styles.matchDefeat}`}>
+      <span className={`${styles.resultTag} ${victory ? styles.victoryText : styles.defeatText}`}>{victory ? "Victory" : "Defeat"}</span>
       <p>Map: {match.meta.map.name} ...... Mode: {match.meta.mode}</p>
     </li>
   );
