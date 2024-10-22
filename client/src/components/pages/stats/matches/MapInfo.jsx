463931bb828c67512a636bfa6080fdecd116fbb1
Mark Kravchuk <markkravchuk123@icloud.com>
2024-10-21 18:52:28 -0700

    Center MapInfo

--- client/src/components/pages/stats/matches/MapInfo.jsx
+++ client/src/components/pages/stats/matches/MapInfo.jsx
@@ -4,7 +4,6 @@ const MapInfo = ({ victory, roundsWon, roundsLost, name }) => {
   return (
     <div className={styles.mapInfo}>
         <span>{victory ? "Victory" : "Defeat"}</span>
-        <img src="map.png" alt="" />
         <span className={styles.roundDiff}>{roundsWon} : {roundsLost}</span>
         <span>{name}</span>
     </div>
