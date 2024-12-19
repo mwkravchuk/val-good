d040f8aeb73149101823f5c5fdbf3cd9e0d8f1a9
Mark Kravchuk <markkravchuk123@icloud.com>
2024-12-19 13:42:58 -0800

    Style ranked history

--- client/src/components/pages/stats/general/GeneralStats.jsx
+++ client/src/components/pages/stats/general/GeneralStats.jsx
@@ -6,11 +6,11 @@ import styles from "./GeneralStats.module.css";
 
 const GeneralStats = ({ playerMMR }) => {
   return (
-    <>
+    <div className={styles.container}>
       <RankedHistory playerMMR={playerMMR} />
       <ActivityHeatmap height="250px"/>
       <Accuracy height="150px"/>
-    </>
+    </div>
   );
 };
 
